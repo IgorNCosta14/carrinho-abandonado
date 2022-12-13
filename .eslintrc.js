b3416a884ddbc1b704d16da099c264bb2bc8c33e
Igor Nascimento Costa <igorncosta14@hotmail.com>
2022-12-13 14:23:45 -0300

    update: add webpack

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,12 @@ module.exports = {
     es2021: true,
     node: true
   },
+  files: ['**/*.ts?(x)'],
   extends: 'standard-with-typescript',
   overrides: [
   ],
+  parser: 'typescript-eslint-parser',
+  plugins: ['typescript'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module'
