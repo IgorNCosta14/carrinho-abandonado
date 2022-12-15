b093cfdfcba3834cb728d425cbb42b1a4bc44e79
Igor Nascimento Costa <69372874+IgorNCosta14@users.noreply.github.com>
2022-12-15 01:01:06 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -24,7 +24,7 @@ io.on('connection', (socket) => {
   console.log(`New connection: ${socket.id}`)
 
   socket.on('sendAbandonedCartInfo', (data) => {
-    let dataToSend = data
+    var dataToSend = data
 
     async function sendCartInfo () {
 
